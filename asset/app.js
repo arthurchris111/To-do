21bf3b83c683f0bd577e46ec38fa1e1304293541
christian <arthurchristian111@gmail.com>
2022-09-03 18:24:27 +0000

    demo

--- asset/app.js
+++ asset/app.js
@@ -3,10 +3,11 @@ const cancelButton = document.getElementById("cancelBtn");
 const title = document.getElementById("title");
 const description = document.getElementById("description");
 const todoForm = document.getElementById("todoForm");
-const deleteBtn = document.querySelectorAll(".deleteBtn");
+// const deleteBtn = document.querySelectorAll(".deleteBtn");
 const todoItem = document.querySelector(".todoItem");
 // const ul = document.querySelector("todo-list");
 const deleteSpan = document.getElementById("deleteSpan");
+const checkBox = document.getElementById("checkBox");
 
 
 var todoList = [];
@@ -35,91 +36,65 @@ addBtn.addEventListener("click", (event) => {
     todoSelector();
 
     todoForm.reset();
-
-
 })
 
-
-
-
 const todoSelector = () => {
-    if (todoList.length == 0) {
-        return
-    }
+    // if (todoList.length == 0) {
+    //     return
+    // }
     const todoItemSelector = document.querySelector(".todoList")
     const todoItem = document.querySelector(".todoItem")
+
 
     todoList.map((item) => {
         todoItemSelector.innerHTML =
             `<div class="todoItem  d-flex align-items-center" id="demo">
                                 <div class="flex-shrink-0">
-                                    <input type="checkbox" class="todo-input done-icon"> 
+                                    <input type="checkbox" class="todo-input" id="checkBox" onclick="strickText()"> 
                                 </div>
                                 <div class="list flex-grow-1 ms-3 d-flex justify-content-between align-items-center">
                                     <ul class = "ul list-unstyled m-0 ">
-                                        <li class="todo-text">${item.title}</li>
-                                        <li class="todo-text d-flex justify-content-between align-items-center">${item.description}s</li>                                
+                                        <li id="todoText">${item.title}</li>
+                                        <li id="todoText" class="d-flex justify-content-between align-items-center">${item.description}</li>                                
                                     </ul>
                                     
-                                    <span id="deleteSpan" border-0 bg-transparent" onclick="removeItem()">
-                                        <img src="./asset/trash-bin.png" alt="delete" width="25" height="25" class="deleteBtn">
+                                    <span id="deleteSpan" class="border-0 bg-transparent" onclick="removeItem()">
+                                        <img src="./asset/trash-bin.png" alt="delete" width="25" height="25">
                                     </span>             
                                 </div>
-                             <hr>
-                            `
-
+                             <hr>`
     })
     todoItemSelector.appendChild(todoItem)
+}
+
+//strick-through inputValues
+const strickText = () => {
+    // var todoText = document.getElementsByClassName("todoText");
+    // for (var i in todoText)
+    //     todoText[i].style.textDecoration = 'line-through'
+    // if (document.querySelector("#checkBox").checked) {
+    //     document.querySelector("#todoText").style.textDecoration = "line-through"
+    // }
 
 }
 
 
+
+
+//clear input field
+const clearFields = () => {
+    title.value = ""
+    description.value = ""
+}
+
+
+//remove-item
 const removeItem = () => {
     const element = document.getElementById("demo");
     element.remove();
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//validation
 todoForm.addEventListener('input', (event) => {
     switch (event.target.id) {
         case 'title':
