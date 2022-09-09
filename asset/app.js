165a15699d227a1f12d65a590870086b588ad7a6
christian <arthurchristian111@gmail.com>
2022-09-09 00:51:00 +0000

    strikeThrough

--- asset/app.js
+++ asset/app.js
@@ -3,139 +3,98 @@ const cancelButton = document.getElementById("cancelBtn");
 const title = document.getElementById("title");
 const description = document.getElementById("description");
 const todoForm = document.getElementById("todoForm");
-// const deleteBtn = document.querySelectorAll(".deleteBtn");
 const todoItem = document.querySelector(".todoItem");
-// const ul = document.querySelector("todo-list");
 const deleteSpan = document.getElementById("deleteSpan");
 const checkBox = document.getElementById("checkBox");
 
 
-var todoList = [];
+var todo = [];
+var todoListView = []
 var inputValues = {}
 
-addBtn.addEventListener("click", (event) => {
-    event.preventDefault()
+
+
+addButton.addEventListener('click', () => {
+    const input = document.querySelector('#input');
 
     titleValidation();
     descriptionValidation();
-
-    // isFormValid();
-
     const isFormValid = (descriptionValidation() && titleValidation() == true)
     if (isFormValid == false) {
         return
     }
+
 
     inputValues = {
         "title": title.value,
         "description": description.value
     }
 
-    todoList.push(inputValues);
+    if (inputValues) {
+        addToList(inputValues);
+        todoForm.reset();
+    }
 
-    todoSelector();
-
-    todoForm.reset();
-})
-
-const todoSelector = () => {
-    // if (todoList.length == 0) {
-    //     return
-    // }
-    const todoItemSelector = document.querySelector(".todoList")
-    const todoItem = document.querySelector(".todoItem")
+});
 
 
-    todoList.map((item) => {
-        todoItemSelector.innerHTML =
-            `<div class="todoItem d-flex align-items-center" id="demo">
-                                <div class="flex-shrink-0">
-                                    <input type="checkbox" class="todo-input" id="checkBox" onclick="strickText()"> 
-                                </div>
-                                <div class="list flex-grow-1 ms-3 d-flex justify-content-between align-items-center">
-                                    <ul class = "list-unstyled m-0" id="ul">
-                                        <li class="todoText">${item.title}</li>
-                                        <li class="todoText class="d-flex justify-content-between align-items-center">${item.description}</li>                                
-                                    </ul>
-                                    
-                                    <span id="deleteSpan" class="border-0 bg-transparent" onclick="removeItem()">
-                                        <img src="./asset/trash-bin.png" alt="delete" width="25" height="25">
-                                    </span>             
-                                </div>
-                             <hr>`
-    })
-    todoItemSelector.appendChild(todoItem)
+
+const addToList = (task) => {
+    const list = document.querySelector('#list');
+    const id = new Date().getMilliseconds();
+
+    list.innerHTML += `<div class="todoItem d-flex align-items-center" id="${id}">
+                             <div class="flex-shrink-0">
+                                 <input type="checkbox" class="todo-input" id="checkBox_${id}" onclick="strikeText(${id})"> 
+                             </div>
+                             <div class="list flex-grow-1 ms-3 d-flex justify-content-between align-items-center">
+                                 <ul class="ul list-unstyled m-0" id="ul_${id}">
+                                     <li class="todoText" id="li">${task.title}</li>
+                                     <li li class = "todoText d-flex justify-content-between align-items-center" id="li">${task.description}</li>                                
+                                 </ul>
+
+                                 <span id="delete"class = "border-0 bg-transparent" onclick="removeItem(${id})">
+                                     <img src="./asset/trash-bin.png" alt="delete" width="25" height="25">
+                                 </span>             
+                             </div>
+                          <hr>`
+
+    console.log(list)
 }
 
-//strick-through inputValues
-const strickText = () => {
-    var todoText = document.getElementsByClassName("todoText");
-    const checkBox = document.getElementById("checkBox");
 
-    // for (var i in todoText)
-    //     todoText[i].style.textDecoration = 'line-through'
+//strick-through inputValues
+const strikeText = (id) => {
+    console.log(id, '======id')
+    const checkBox = document.getElementById("checkBox_" + id);
+    console.log(checkBox, '======checkbox')
     if (checkBox.checked) {
-        document.getElementById("ul").style.textDecorationLine = "line-through";
+        document.getElementById("ul_" + id).style.textDecorationLine = "line-through";
     } else {
-        document.getElementById("ul").style.textDecorationLine = "none";
+        document.getElementById("ul_" + id).style.textDecorationLine = "none";
     }
 
 }
+
+//remove-item
+const removeItem = (id) => {
+    console.log(id)
+    const element = document.getElementById(id);
+    element.remove();
+
+}
+
+//clear todo list
+function clearTodoList() {
+    list.innerHTML = ""
+}
+
 
 //clear input field
 const clearFields = () => {
     title.value = ""
     description.value = ""
 }
-
-
-//remove-item
-const removeItem = () => {
-    const element = document.getElementById("demo");
-    element.remove();
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
